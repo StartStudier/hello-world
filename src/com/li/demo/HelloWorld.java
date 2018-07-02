707160854c8e093f77e110c0296338a8fb9257f5
lwp <616068917@qq.com>
2018-07-02 19:45:46 +0800

    Initial commit

---
+++ src/com/li/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.li.demo;
+
+/**
+ * Created by LI on 2018/7/2.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.print("hello world!");
+    }
+}
